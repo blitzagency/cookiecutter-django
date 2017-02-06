99abaa5ad0ccd8ffc7fecdf2760f9e096295be81
dinopetrone <dinopetrone@gmail.com>
2017-02-06 13:00:47 -0800

    urls

--- django/project/app/config/urls.py
+++ django/project/app/config/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 
