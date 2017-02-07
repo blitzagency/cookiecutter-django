0f5d41bda041c482421f3c6f1e25c13f5e178637
dinopetrone <dinopetrone@gmail.com>
2017-02-07 08:23:32 -0800

    removing require from storages setup

--- django/project/app/utils/storage.py
+++ django/project/app/utils/storage.py
@@ -4,14 +4,12 @@ from django.contrib.staticfiles.storage import CachedFilesMixin
 
 from storages.backends.s3boto import S3BotoStorage
 
-from require.storage import OptimizedFilesMixin
 
-
-class OptimizedCachedS3BotoStorage(OptimizedFilesMixin, CachedFilesMixin, S3BotoStorage):
+class OptimizedCachedS3BotoStorage(CachedFilesMixin, S3BotoStorage):
     pass
 
 
-class OptimizedS3BotoStorage(OptimizedFilesMixin, S3BotoStorage):
+class OptimizedS3BotoStorage(S3BotoStorage):
 
     def __init__(self, *args, **kwargs):
         if hasattr(settings, "AWS_CLOUDFRONT_DOMAIN"):
