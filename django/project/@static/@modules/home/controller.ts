2ea384612b268aaacb52a817856ab08a264f2a56
Aubrey Taylor <aubricus@gmail.com>
2017-02-08 19:42:59 -0800

    chore(nunjucks): Remove test rig

--- django/project/@static/@modules/home/controller.ts
+++ django/project/@static/@modules/home/controller.ts
@@ -2,12 +2,7 @@ import * as Backbone from "backbone";
 import * as Marionette from "backbone.marionette";
 import * as _ from "underscore";
 import {on, Options} from "../utils/decorators";
-import TestView from "./views/test-view";
 
-/**
- * This controller is a good spot to initialize any javascript comonents
- * that might need setup for demo in the UI Docs.
- */
 
 @Options({
     template: false,
@@ -24,6 +19,6 @@ export default class ViewController extends Marionette.View<Backbone.Model>{
     onRender() {
         // Temp
         console.log("Home ViewController Rendered!");
-        (<any>this).showChildView("main", new TestView());
+
     }
 }
