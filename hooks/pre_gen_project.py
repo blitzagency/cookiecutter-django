2b4a7630b9ccb006fcd7a9ec53cee5dcb077075a
Aubrey Taylor <aubricus@gmail.com>
2017-05-10 18:52:20 -0700

    feature(heroku): Improve Heroku scripts / docs
    Summary:
    - Update cookiecutter.json with heroku_slug
    - Update cookiecutter.json to derive a valid heroku_slug from
      project_slug
    - Update pre_gen_project to check for a valid heroku_slug
    - Update app_info.json to use heroku_slug instead of project_slug
    - Update README.md with heroku deploy info
    - Update README.md to use heroku_slug
    - DRY-up Makefile scripts
    - Support deploying to 'dev'
    - Remove outdated setup doc
    - Add new setup doc
    - Add gen_secret_key mgmt command
    - Delete out of date DEPLOYMENT_NOTES.md
    - Add updated heroku-setup.md
    - Update .gitignore to ignore cors.json

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,10 @@
 project_slug = "{{ cookiecutter.project_slug }}"
+heroku_slug = "{{cookiecutter.heroku_slug}}"
+
 
 if hasattr(project_slug, "isidentifier"):
     assert project_slug.isidentifier(), "Project slug should be valid Python identifier!"
+
+
+if heroku_slug:
+    assert "_" not in heroku_slug, "Heroku slugs / app names must not have underscores!"
