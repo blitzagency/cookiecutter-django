807dddbd17f7796adc26037d6d4577765f1aa235
jeffrey k eliasen <jeff@jke.net>
2018-01-26 14:03:36 -0700

    fix: enable middleware

--- {{cookiecutter.project_slug}}/django/project/app/config/settings/base.py
+++ {{cookiecutter.project_slug}}/django/project/app/config/settings/base.py
@@ -148,6 +148,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.redirects.middleware.RedirectFallbackMiddleware",
 
     # DjangoCMS
     "cms.middleware.user.CurrentUserMiddleware",
